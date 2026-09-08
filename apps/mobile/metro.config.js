9aaaee91c45bf9224962b8423854a03281f05dc9
chitta-dev <chittaranjancrp@gmail.com>
2026-09-09 02:29:49 +0530

    feat: add interactive mobile app simulator tab for instant zero-wait in-browser testing

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,5 +1,14 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+// Ignore external apps and build folders
+config.resolver.blockList = [
+  /.*[\/\\]apps[\/\\]web-admin[\/\\].*/,
+  /.*[\/\\]apps[\/\\]backend[\/\\].*/,
+  /.*[\/\\]dist[\/\\].*/,
+  /.*[\/\\]build[\/\\].*/,
+];
 
 module.exports = config;
