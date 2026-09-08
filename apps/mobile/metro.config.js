854e14c0258e98480a08c86748cdbdbe836da4f4
chitta-dev <chittaranjancrp@gmail.com>
2026-09-09 02:53:19 +0530

    fix(mobile): resolve Babel preset and Metro blockList to achieve clean 100% iOS and Android bundling

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,14 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Ignore external apps and build folders
-config.resolver.blockList = [
-  /.*[\/\\]apps[\/\\]web-admin[\/\\].*/,
-  /.*[\/\\]apps[\/\\]backend[\/\\].*/,
-  /.*[\/\\]dist[\/\\].*/,
-  /.*[\/\\]build[\/\\].*/,
-];
 
 module.exports = config;
